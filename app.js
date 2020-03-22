1814cf66e193235dd9604765c59f5b58d6e44ec2
Nathaniel Alcantara <griffinsnathaniel@gmail.com>
2020-03-22 11:54:52 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendfile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const emailAdd = req.body.emailAdd;
+
+  const data = {
+    members: [
+      {
+        email_address: emailAdd,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us11.api.mailchimp.com/3.0/lists/9d361fe037";
+  const options = {
+    method: "POST",
+    auth: "negroshaman:ccff0a4be8c34a91b92fd061c7129109-us19"
+  };
+  const request = https.request(url, options, function(response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendfile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(req, res) {
+  console.log("server is runnin on PORT 3000.");
+});
+
+// API KEY
+// ccff0a4be8c34a91b92fd061c7129109-us19
+
+// LIST ID
+// 9d361fe037
